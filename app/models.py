05d507e28f1ffa8f70ad3a48f56cae3a4f965487
Liam Andrew <landrew@texastribune.org>
2016-01-09 16:26:36 -0500

    Filled out models and main template

--- app/models.py
+++ app/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=128)
+
+class Quote(models.Model):
+    book = models.ForeignKey(Book)
+    text = models.TextField()
+
+class Location(models.Model):
+    book = models.ForeignKey(Book)
+    lat = models.FloatField()
+    lon = models.FloatField()
