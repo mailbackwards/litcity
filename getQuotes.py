8c3756615fb93a5c5eef118ca3bf2de6d9695fcf
Navie Narula <navrajnarula@gmail.com>
2016-01-10 10:01:52 -0500

    add files

--- getQuotes.py
+++ getQuotes.py
@@ -6,18 +6,22 @@
 # This script uses BeautifulSoup-4 to perform the final html parsing to retrieve the quotes and
 # a python wrapper for the Goodreads API to retrieve the relevant bookID from the given string raw_input
 
-# Link to the python wrapper: https://github.com/sefakilic/goodreads 
+# Link to the python wrapper: https://github.com/sefakilic/goodreads
 
+#                                       ---
+
+# This file was borrowed from: https://github.com/mon95/Goodreads-Quotes-Extractor/blob/master/getQuotes.py
+
+# import libraries
 from goodreads import client
 from goodreads import book
 import re
 import requests
 import urllib
-
 from bs4 import BeautifulSoup
 
-
-def cleanedUpQuote(quote):              # To remove stray html tags from the retrieved results
+# clean up quotes
+def cleanedUpQuote(quote):       
     quote = re.sub('<.*?>','',quote)
     return quote
 
@@ -25,33 +29,23 @@ def cleanedUpQuote(quote):              # To remove stray html tags from the ret
 CONSUMER_KEY = "AL6GKEoebHFenfoDmtHuOA"
 CONSUMER_SECRET ="WFelKpMYYwRTOelTyJF449HYuMadDBflYaocGQh4lqk"
 
-
 gc = client.GoodreadsClient(CONSUMER_KEY,CONSUMER_SECRET)
 
-#gc.authenticate()
+#gc.authenticate() -- OAuth may be unneccasary
 
 bookName = raw_input("Enter the name of your favorite book(Enter Author's name to retrive top quotes from the author): ")
 
 print "Hold on while we retrieve the top quotes..."
 
-#bookIdList = gc.search_books(bookName)
-#How to declare bookName as an instance of book. bookname = book(x, x, x)
 r = requests.get('https://www.goodreads.com/book/title.xml?key=%s&title=%s' % (CONSUMER_KEY, bookName))
-print r
 soup = BeautifulSoup(r.text)
 book_id = soup.find('book').find('work').find('id').text
-print book_id
 baseUrl = 'https://www.goodreads.com/work/quotes/'
 editedBookName = bookName.replace(' ','-')
 s = book_id+'-'+editedBookName
-print "s is", s
-print "editedbookname is", editedBookName
+# construct the final URL that will retrieve quotes
 finalUrl = str(baseUrl) + str(s);
-#finalUrl =  "https://www.goodreads.com/work/quotes/2368224-Ulysses"
-print "finalURL is: ", finalUrl
 
-
-# print finalUrl
 
 print "..............."
 print 
@@ -60,7 +54,6 @@ print
 html = urllib.urlopen(finalUrl).read()
 soup = BeautifulSoup(html,"lxml")
 
-# print soup
 quotesPart = soup.findAll("div",class_="quoteText")
 
 for item in quotesPart:
---
+++ goodreads/request.py
@@ -0,0 +1,31 @@
+import requests
+import xmltodict
+import json
+
+class GoodreadsRequestException(Exception):
+    def __init__(self, error_msg, url):
+        self.error_msg = error_msg
+        self.url = url
+
+    def __str__(self):
+        return self.url, ':', self.error_msg
+
+class GoodreadsRequest():
+    def __init__(self, client, path, query_dict, req_format='xml'):
+        """Initialize request object."""
+        self.params = dict(query_dict.items() + client.query_dict.items())
+        self.host = client.base_url
+        self.path = path
+        self.req_format = req_format
+
+    def request(self):
+        resp = requests.get(self.host+self.path, params=self.params)
+        if resp.status_code != 200:
+            raise GoodreadsRequestException(resp.reason, self.path)
+        if self.req_format == 'xml':
+            data_dict = xmltodict.parse(resp.content)
+            return data_dict['GoodreadsResponse']
+        elif self.req_format == 'json':
+            return json.loads(resp.content)
+        else:
+            raise Exception("Invalid format")
