e531b011b4680a4b4181c3ac577b6aba940d5a86
Liam Andrew <landrew@texastribune.org>
2016-01-09 19:23:52 -0500

    How do stupid static files work

--- litcity/settings.py
+++ litcity/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'app', 'static'),
 )
